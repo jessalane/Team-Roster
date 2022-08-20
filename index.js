93b915746787b32342bd6a48d8b83bbccf0d16d0
Jessica Lane <jessica.lane.a@gmail.com>
2022-08-19 20:03:07 -0600

    updating with questions

--- index.js
+++ index.js
@@ -1,20 +1,102 @@
 // requried packages and imported files
 const inquirer = require('inquirer');
 const fs = require('fs');
-let renderHTML = require('./assets/js/renderOutput.js');
+const renderHTML = require('./assets/js/renderOutput.js');
 
-// array of questions
-const questions = [
+// arrays of questions
+const roleQ = [
     {
         type: 'list',
-        message: 'Would you like to allow contributions?',
-        name: 'contribute',
-        choices: ['yes', 'no']
+        message: 'What is this role for this employee?',
+        name: 'empRole',
+        choices: ['manager', 'engineer', 'employee', 'intern']
+    },
+];
+
+const employeeQ = [
+    {
+        type: 'input',
+        message: 'What the name of this employee?',
+        name: 'empRole'
     },
     {
         type: 'input',
-        message: 'How do you test this application?',
-        name: 'tests',
+        message: 'What the email of this employee?',
+        name: 'empEmail'
+    },
+    {
+        type: 'input',
+        message: 'What the id of this employee?',
+        name: 'empId'
+    },
+];
+
+const engineerQ = [
+    {
+        type: 'input',
+        message: 'What the name of this engineer?',
+        name: 'engRole'
+    },
+    {
+        type: 'input',
+        message: 'What the email of this engineer?',
+        name: 'engEmail'
+    },
+    {
+        type: 'input',
+        message: 'What the id of this engineer?',
+        name: 'engId'
+    },
+    {
+        type: 'input',
+        message: 'What the github name of this engineer?',
+        name: 'engGit'
+    },
+];
+
+const managerQ = [
+    {
+        type: 'input',
+        message: 'What the name of this manager?',
+        name: 'manRole'
+    },
+    {
+        type: 'input',
+        message: 'What the email of this manager?',
+        name: 'manEmail'
+    },
+    {
+        type: 'input',
+        message: 'What the id of this manager?',
+        name: 'manId'
+    },
+    {
+        type: 'input',
+        message: 'What the store number name of this manager?',
+        name: 'manStore'
+    },
+];
+
+const internQ = [
+    {
+        type: 'input',
+        message: 'What the name of this intern?',
+        name: 'intRole'
+    },
+    {
+        type: 'input',
+        message: 'What the email of this intern?',
+        name: 'intEmail'
+    },
+    {
+        type: 'input',
+        message: 'What the id of this intern?',
+        name: 'intId'
+    },
+    {
+        type: 'input',
+        message: 'What the school name of this intern?',
+        name: 'intSchool'
     },
 ];
 
@@ -26,7 +108,7 @@ function writeToFile(data) {
 // initialize funciton to run the questions
 function init() {
     try {
-        inquirer.prompt(questions)
+        inquirer.prompt(roleQ)
         .then((answer) => {
             const genHTML = renderHTML(answer);
             writeToFile(genHTML);
